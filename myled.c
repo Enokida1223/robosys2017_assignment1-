9d5cfba613e4aaf5bd96c89df13a9a9c79bd2564
Enokida1223 <34084590+Enokida1223@users.noreply.github.com>
2017-12-31 10:10:59 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -32,7 +32,7 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 static ssize_t sushi_read(struct file* filp, char* buf, size_t count, loff_t* pos)
 {
         int size = 0;
-        char sushi[] = "SUSHI\n"; //寿司の絵文字のバイナリ
+        char sushi[] = "寿司\n"; 
         if(copy_to_user(buf+size,(const char *)sushi, sizeof(sushi))){
                 printk( KERN_INFO "sushi : copy_to_user failed\n" );
                 return -EFAULT;
